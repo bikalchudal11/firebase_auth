c155e7897442659f8eb66c7eca2e9157fc8fe2cc
bikalchudal11 <bikalchudal11@gmail.com>
2024-03-06 08:08:19 +0545

    fetching data from from firebase database done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_series/home.dart';
-import 'package:firebase_series/screens/email_auth/login_screen.dart';
 import 'package:firebase_series/screens/phone_auth/sign_in_with_phone.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -13,6 +13,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  //for fetching data using firebase database
+  // DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //     .collection("users")
+  //     .doc("uFnzU8mugY50jogITkNU")
+  //     .get();
+  // print(snapshot.data().toString());
+
   runApp(MyApp());
 }
 
